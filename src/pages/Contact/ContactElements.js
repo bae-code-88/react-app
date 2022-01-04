3cc3b89f68d88908dd261e6915cc26e6430af29c
Beata Krawiec <beata2krawiec@wp.pl>
2022-01-04 19:17:11 +0100

    four

--- src/pages/Contact/ContactElements.js
+++ src/pages/Contact/ContactElements.js
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaPhoneAlt,
+  FaEnvelope,
+} from "react-icons/fa";
 
 export const ContactBox = styled.div`
   position: absolute;
@@ -8,7 +14,7 @@ export const ContactBox = styled.div`
   height: 80vh;
 
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
@@ -21,11 +27,14 @@ export const ContactLeftContainer = styled.div`
 
   height: 100%;
   width: 40%;
+  @media screen and (max-width: 1280px) {
+    width: 100%;
+  }
 `;
 
 export const ContactStyledLetter = styled.h1`
   position: absolute;
-  top: -200px;
+  top: -160px;
   left: -2%;
 
   font-size: 990px;
@@ -52,7 +61,7 @@ export const ContactStyledLetter = styled.h1`
 
 export const ContactMainIntro = styled.h1`
   position: absolute;
-  top: 30%;
+  top: 5%;
   left: 10%;
 
   font-size: 7rem;
@@ -87,12 +96,12 @@ export const ContactMainIntro = styled.h1`
 
 export const ContactMainInfo = styled.p`
   position: absolute;
-  top: 55%;
+  top: 25%;
   left: 10%;
 
   z-index: 1;
   width: 35%;
-  font-size: 20px;
+  font-size: 16px;
   line-height: 30px;
 
   @media screen and (max-width: 1366px) {
@@ -119,4 +128,198 @@ export const ContactMainInfo = styled.p`
     top: 65%;
     font-size: 16px;
   }
+`;
+
+export const ContactIcons = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 10%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 18px;
+
+  @media screen and (max-width: 785px) {
+    margin-left: 0;
+  }
+  @media screen and (max-width: 1246px) {
+    margin-top: 20px;
+  }
+`;
+
+export const ContactIconBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 18px;
+
+  &:nth-child(2) {
+    margin-left: 1rem;
+  }
+
+  @media screen and (max-width: 785px) {
+    margin-left: 0;
+  }
+  @media screen and (max-width: 1246px) {
+    margin-top: 20px;
+  }
+`;
+export const ContactIconPhone = styled(FaPhoneAlt)`
+  display: block;
+  font-size: 1.1rem;
+  color: #a85f3f;
+  margin: 0 0.9rem 0 0;
+`;
+export const ContactIconMail = styled(FaEnvelope)`
+  display: block;
+  font-size: 1.1rem;
+  color: #a85f3f;
+  margin: 0 0.9rem 0 0;
+`;
+
+export const ContactTitleAdress = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 10%;
+  color: #a85f3f;
+  font-size: 14px;
+  align-self: flex-start;
+`;
+export const ContactAdress = styled.p`
+  position: absolute;
+  top: 55%;
+  left: 10%;
+  font-size: 14px;
+`;
+
+export const RegisterInformationTitle = styled.p`
+  position: absolute;
+  top: 65%;
+  left: 10%;
+  font-size: 14px;
+  color: #a85f3f;
+`;
+
+export const RegisterInformationName = styled.p`
+  position: absolute;
+  top: 70%;
+  left: 10%;
+  font-size: 14px;
+`;
+
+export const RegisterInformationAdress = styled.p`
+  position: absolute;
+  top: 75%;
+  left: 10%;
+  font-size: 14px;
+`;
+
+export const RegisterInformationNumbers = styled.p`
+  position: absolute;
+  top: 80%;
+  left: 10%;
+  font-size: 14px;
+`;
+
+export const RegisterAccountNumber = styled.p`
+  position: absolute;
+  top: 85%;
+  left: 10%;
+  font-size: 14px;
+`;
+
+export const IconsMediaBox = styled.div`
+  position: absolute;
+  top: 95%;
+  left: 10%;
+  width: 50%;
+
+  display: flex;
+  flex-direction: row;
+  justify-items: space-between;
+  font-size: 14px;
+`;
+export const IconFacebook = styled(FaFacebookF)`
+  display: block;
+  font-size: 1rem;
+  cursor: pointer;
+  color: #1d1d1b;
+`;
+export const IconInstagram = styled(FaInstagram)`
+  display: block;
+  font-size: 1rem;
+  cursor: pointer;
+  color: #1d1d1b;
+  margin-left: 1.8rem;
+`;
+
+export const ContactRightContainer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  width: 50%;
+  height: 100%;
+
+  @media screen and (max-width: 1280px) {
+    width: 100%;
+  }
+`;
+
+export const ContactFormBoxTitle = styled.p`
+  display: block;
+  font-size: 35px;
+  width: 80%;
+  margin: 10% 0 4% 0;
+`;
+
+export const ContactFormBoxText = styled.p`
+  display: block;
+  font-size: 16px;
+  width: 80%;
+  margin: 0 0 5% 0;
+`;
+export const ContactFormBox = styled.form`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContactFormInput = styled.input`
+  width: 70%;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid #a85f3f;
+  line-height: 40px;
+`;
+
+export const ContactMessageBox = styled.textarea`
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid #a85f3f;
+  line-height: 30px;
+  resize: none;
+`;
+
+export const ContactFormButton = styled.button`
+  width: 100px;
+  height: 40px;
+  color: white;
+  background-color: #a85f3f;
+  border: none;
+  margin-top: 10%;
+  cursor: pointer;
+`;
+
+export const ContactMapPlace = styled.div`
+  position: absolute;
+  top: 100vh;
+  width: 80%;
+  height: 120vh;
 `;
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,7 +4,31 @@ import {
   ContactStyledLetter,
   ContactMainIntro,
   ContactMainInfo,
+  ContactIcons,
+  ContactIconBox,
+  ContactIconPhone,
+  ContactIconMail,
+  ContactTitleAdress,
+  ContactAdress,
+  RegisterInformationTitle,
+  RegisterInformationName,
+  RegisterInformationAdress,
+  RegisterInformationNumbers,
+  RegisterAccountNumber,
+  IconFacebook,
+  IconInstagram,
+  IconsMediaBox,
+  ContactRightContainer,
+  ContactFormBox,
+  ContactFormInput,
+  ContactMessageBox,
+  ContactFormBoxTitle,
+  ContactFormBoxText,
+  ContactFormButton,
+  ContactMapPlace,
 } from "./ContactElements";
+
+import ContactMap from "./ContactMap";
 
 const Contact = () => {
   return (
@@ -14,10 +38,57 @@ const Contact = () => {
           <ContactStyledLetter>K</ContactStyledLetter>
           <ContactMainIntro>Kontakt</ContactMainIntro>
           <ContactMainInfo>
-            Nasz zespół pozostaje do Twojej dyspozycji, skontaktuj się z nami a
-            znajdziemy idealne rozwiązanie.
+            Nasz zespół pozostaje do Twojej dyspozycji, skontaktuj się z nami
+            <br></br>a znajdziemy idealne rozwiązanie.
           </ContactMainInfo>
+          <ContactIcons>
+            <ContactIconBox>
+              <ContactIconPhone />+ 48 793 033 088
+            </ContactIconBox>
+            <ContactIconBox>
+              <ContactIconMail />
+              kontakt@kontaktdesign.pl
+            </ContactIconBox>
+          </ContactIcons>
+          <ContactTitleAdress>Adres pracowni</ContactTitleAdress>
+          <ContactAdress>ul. Nieznana 8 bud.13, 04-690 Warszawa</ContactAdress>
+          <RegisterInformationTitle>Dane rejestrowe</RegisterInformationTitle>
+          <RegisterInformationName>NoName sp.z o.o.</RegisterInformationName>
+          <RegisterInformationAdress>
+            ul. Nieznana 8 bud.13, 04-690 Warszawa
+          </RegisterInformationAdress>
+          <RegisterInformationNumbers>
+            KRS 1000000000, NIP 20 00000000, REGON 300000000
+          </RegisterInformationNumbers>
+          <RegisterAccountNumber>
+            Nr konta: 12 3456 7890 0000 1111 2222 3333
+          </RegisterAccountNumber>
+          <IconsMediaBox>
+            <IconFacebook /> <IconInstagram />
+          </IconsMediaBox>
         </ContactLeftContainer>
+        <ContactRightContainer>
+          <ContactFormBoxTitle>Zostaw wiadomość</ContactFormBoxTitle>
+          <ContactFormBoxText>Czekamy na Twoją wiadomość.</ContactFormBoxText>
+          <ContactFormBox name="contact">
+            <ContactFormInput placeholder="Imię i nazwisko"></ContactFormInput>
+            <ContactFormInput placeholder="Telefon"></ContactFormInput>
+            <ContactFormInput
+              type="email"
+              placeholder="E-mail"
+            ></ContactFormInput>
+            <ContactMessageBox
+              name="contact"
+              placeholder="Wiadomość"
+              rows="5"
+              cols="60"
+            ></ContactMessageBox>
+            <ContactFormButton>Wyślij</ContactFormButton>
+          </ContactFormBox>
+        </ContactRightContainer>
+        <ContactMapPlace>
+          <ContactMap />
+        </ContactMapPlace>
       </ContactBox>
     </>
   );
